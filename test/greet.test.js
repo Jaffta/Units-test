e42583e1c127fe82f6f27d068f6c21f2e9279e1f
Jafta <201435755@student.uj.ac.za>
2022-08-23 13:11:34 +0200

    add myfiles

---
+++ test/greet.test.js
@@ -0,0 +1,5 @@
+describe ('It should test the greet function', function(){
+    it('It should greet Jay with "Hello, Jay"', function(){
+        assert.equal('Hello, Jay', greet('Jay'))
+    });
+}) 
